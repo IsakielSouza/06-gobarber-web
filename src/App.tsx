64153501ff4cb3ca47174bbfad7fa7d2b7865a7e
Isakiel Souza <isakielsouza@outlook.com.br>
2020-06-14 17:05:36 -0300

    Usabilidade do input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import SingIn from './pages/SignIn';
+// import SingIn from './pages/SignIn';
 import SignUp from './pages/signUp';
 
 const App: React.FC = () => (
