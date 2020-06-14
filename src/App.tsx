5e70c7c9b4749b92cd0e503350818f1f55f0ddac
Isakiel Souza <isakielsouza@outlook.com.br>
2020-06-14 15:12:36 -0300

    Criado pagina de SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 import SingIn from './pages/SignIn';
+import SignUp from './pages/signUp';
 
 const App: React.FC = () => (
   <>
-    <SingIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
